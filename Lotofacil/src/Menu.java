9271d5c3a6526ccc8fd538a9cfc66df35a95cee1
Otavio Silva <estudomafia@gmail.com>
2023-09-20 00:50:18 -0300

    Implementando solução do menu da loteria

--- Lotofacil/src/Menu.java
+++ Lotofacil/src/Menu.java
@@ -1,13 +1,13 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
 import java.lang.Character;
 
 public class Menu {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         int escolha;
         int numeroAposta;
-        char letraAposta;
 
         do {
             System.out.println("*****************************");
@@ -35,9 +35,36 @@ public class Menu {
                     }
                     break;
                 case 2:
+                    boolean letraValida = false;
+                    char letraVencedora = 'O';
                     System.out.println("Digite uma letra de A até Z");
-                    char letraVencedor = 'O';
-                    letraAposta = scanner.next().charAt(0);
+                    char letraAposta = (char) System.in.read();
+                    while (!letraValida) {
+                        if (Character.isLetter(letraAposta)){
+                            letraValida = true;
+                        } else {
+                            System.out.println("Aposta inválida, tente novamente com uma letra de A até Z");
+                            letraAposta = (char) System.in.read();
+                        }
+
+                    }
+                    letraAposta = Character.toUpperCase(letraAposta);
+                    if (letraAposta == letraVencedora){
+                        System.out.println("Jackpot! Você ganhou R$500,00");
+                    } else {
+                        System.out.println("Que pena! Você perdeu, a letra sorteada foi: " +letraVencedora);
+                    }
+                case 3:
+                    int apostaParImpar;
+                    System.out.println("Insira um número ímpar ou par");
+                    apostaParImpar = scanner.nextInt();
+                    if (apostaParImpar % 2 == 0){
+                        System.out.println("Jackpot! Você ganhou R$100,00");
+                    } else {
+                        System.out.println("Que pena! O número digitado é ímpar e a premiação foi para números pares");
+                    }
+
+
             }
         }
         while (escolha != 0);
